f74851300348c6c2fc1b746520bed5cb2f749490
Maciej Mikulski <maciej.mikulski.jr@gmail.com>
2018-02-15 21:31:27 +0100

    This achieves accuracy 0.919

--- examples/dl-ex3.py
+++ examples/dl-ex3.py
@@ -18,8 +18,11 @@ img_rows, img_cols = 28, 28
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 # limit data
-(x_train, y_train) = (x_train[:6000], y_train[:6000])
-(x_test, y_test) = (x_test[:1000], y_test[:1000])
+# (x_train, y_train) = (x_train[:6000], y_train[:6000])
+# (x_test, y_test) = (x_test[:1000], y_test[:1000])
+# on full data it arrives at 91.9% accuracy, 110 sec/epoch on Intel i7-7700K
+# best hyper-parameters set found by this procedure is
+# [('batch_size', 324), ('momentum', 0.9666666666666667), ('lr', 0.027)]
 
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
@@ -95,7 +98,7 @@ s1.summary()
 best_batch_size = best_point['batch_size']
 
 s2 = Grid()  # in future: Monotonic()
-s2.add(Exponential('batch_size', best_batch_size, 1000), step=3)
+s2.add(Exponential('batch_size', best_batch_size, 2000), step=3)
 s2.add(Dependent('lr', f=lambda batch_size: 0.001 * batch_size / best_batch_size))
 s2.add(Constant('momentum', 0.9))
 
@@ -111,7 +114,7 @@ best_lr = best_point['lr']
 best_batch_size = best_point['batch_size']
 
 s3 = Grid()
-s3.add(Exponential('batch_size', best_batch_size, 2000), step=3)
+s3.add(Exponential('batch_size', best_batch_size, 6000), step=3)
 s3.add(Constant('lr', best_lr))
 s3.add(Dependent('momentum', f=lambda batch_size: 1 - 0.1 * best_batch_size / batch_size))
 
